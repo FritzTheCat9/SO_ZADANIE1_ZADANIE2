d650999a186961e54eaa6e78e82c65a1c505985f
Bartlomiej Uminski <bartlomiejuminski1999@gmail.com>
2020-03-30 14:11:18 +0200

    COMMIT1 - przesylam zadania, prosze o poprawki

---
+++ zadanie1.c
@@ -0,0 +1,62 @@
+// Kompilacja: gcc zadanie1.c -o zadanie1 -pthread
+// Wykonanie: ./zadanie1
+
+// Kompilacja: gcc zadanie1.c -O3 zadanie1 -pthread
+// Wykonanie: ./zadanie1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+// zmienna globalna - licznik
+long long licznik = 0;
+
+// funkcja wykonywana w wątku1
+void* funkcja_watek1(void* arg)
+{
+    while(1)
+    {
+        printf("Watek1 - Licznik: %lld\n", licznik);
+    }
+
+    // skończenie działania wątku
+    pthread_exit(0);
+}
+
+// funkcja wykonywana w wątku2
+void* funkcja_watek2(void* arg)
+{
+    while(1)
+    {
+        printf("Watek2 - Inkrementacja licznika");
+        licznik++;
+    }
+
+    // skończenie działania wątku
+    pthread_exit(0);
+}
+
+int main()
+{
+    // N - liczba wątków
+    int N = 2;
+
+    // ID wątków - tablica
+    pthread_t watek[N];
+
+    // ustawienie domyślnych atrybutów wątków
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+
+    // utworzenie wątkow
+    pthread_create(&watek[0], &attr, funkcja_watek1, NULL);
+    pthread_create(&watek[1], &attr, funkcja_watek2, NULL);
+
+    // czekanie na zakończenie wszystkich wątków
+    for(int i = 0; i < N; i++)
+    {
+        pthread_join(watek[i], NULL);
+    }
+
+    return 0;
+}
---
+++ zadanie2.c
@@ -0,0 +1,81 @@
+// Kompilacja: gcc zadanie2.c -o zadanie2 -pthread
+// Wykonanie: ./zadanie2
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+// struktura do przechowywania informacji z wątków
+struct funkcja_watek_struct
+{
+    int nr_watku;
+    long long liczba_iteracji;
+};
+
+// funkcja wykonywana w wątku1
+void* funkcja_watek(void* arg)
+{
+    struct funkcja_watek_struct* arg_struct = (struct funkcja_watek_struct*) arg;
+    arg_struct->liczba_iteracji = rand()%10000;
+    
+    // PONIŻEJ TEST DLA MNIEJSZYCH  DANYCH
+    /*arg_struct->liczba_iteracji = rand()%3+1;
+    printf("LICZBA ITERACJI WATKU %d = %lld\n", arg_struct->nr_watku, 
+    arg_struct->liczba_iteracji);*/
+
+    for(long long k = 0; k < arg_struct->liczba_iteracji; k++)
+    {
+        printf("Watek %d iteracja %lld\n", arg_struct->nr_watku, k);
+    }
+
+    // skończenie działania wątku
+    pthread_exit(0);
+}
+
+int main()
+{
+    // N - liczba wątków
+    int N = 10;
+
+    // tworzenie struktur na dane z wątków
+    struct funkcja_watek_struct args[N];
+
+    // ID wątków - tablica
+    pthread_t watek[N];
+
+    // ustawienie ziarna do generowania liczb losowych
+    srand(time(NULL));
+
+    // utworzenie wątkow
+    for(int i = 0; i < N; i++)
+    {
+        args[i].nr_watku = i;
+
+        // ustawienie domyślnych atrybutów wątków
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
+
+        pthread_create(&watek[i], &attr, funkcja_watek, &args[i]);
+    }
+
+    long long lacznie_iteracji = 0;
+
+    // czekanie na zakończenie wszystkich wątków
+    for(int i = 0; i < N; i++)
+    {
+        pthread_join(watek[i], NULL);
+        lacznie_iteracji += args[i].liczba_iteracji;
+    }
+
+    // wypisanie liczby iteracji w wątkach
+    for(int i = 0; i < N; i++)
+    {
+        printf("Liczba iteracji w wątku %d: %lld\n", args[i].nr_watku
+        , args[i].liczba_iteracji);
+    }
+
+    printf("Laczna liczba iteracji: %lld\n", lacznie_iteracji);
+
+    return 0;
+}
